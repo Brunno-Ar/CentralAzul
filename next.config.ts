e61fda880f14cc69bf095691749c40ddf1664217
Bruno Araujo <brunnoaraujoc@gmail.com>
2026-06-18 12:01:50 -0300

    fix: revert next.config.ts distDir to default to support cross-drive symlinks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: process.env.NODE_ENV === "development" ? "C:/tmp/central-azul-next" : ".next",
+  /* config options here */
 };
 
 export default nextConfig;
